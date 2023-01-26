e87c5ba6c7710ec1f9efee7962121932c23b1474
krios2146 <dreik2146@gmail.com>
2023-01-26 17:13:21 +0700

    feat: add tests for all methods

--- src/test/java/org/petproject/HangmanGameImplTest.java
+++ src/test/java/org/petproject/HangmanGameImplTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -14,6 +16,8 @@ class HangmanGameImplTest {
 
     @Mock
     private HangmanInterfaceImpl hangmanInterface;
+    @Mock
+    private Dictionary dictionary;
 
     @Spy
     @InjectMocks
@@ -22,6 +26,7 @@ class HangmanGameImplTest {
     @Test
     void initiateGame_ShouldStartTheGame() {
         when(hangmanInterface.askPersonToPlay()).thenReturn("Y");
+        when(dictionary.getWords()).thenReturn(List.of("secret"));
         doNothing().when(hangmanGame).playGame();
 
         hangmanGame.initiateGame();
@@ -31,14 +36,82 @@ class HangmanGameImplTest {
     }
 
     @Test
-    void playGame() {
+    void initiateGame_ShouldExitTheGame() {
+        when(hangmanInterface.askPersonToPlay()).thenReturn("N");
+
+        hangmanGame.initiateGame();
+
+        verify(hangmanInterface).exitGame();
+        verify(hangmanGame).exitGame();
     }
 
     @Test
-    void endGame() {
+    void initiateGame_ShouldCallInitialInterfaceSetupMethods() {
+        when(hangmanInterface.askPersonToPlay()).thenReturn("Y");
+        when(dictionary.getWords()).thenReturn(List.of("secret"));
+        doNothing().when(hangmanGame).playGame();
+
+        hangmanGame.initiateGame();
+
+        verify(hangmanInterface).setLettersIndexes(anyMap());
+        verify(hangmanInterface).setHiddenWord(anyString());
+        verify(hangmanInterface).drawInterface(0);
     }
 
     @Test
-    void exitGame() {
+    void playGame_ShouldLoseTheGame() {
+        when(hangmanInterface.askPersonToPlay()).thenReturn("Y", "N");
+        when(hangmanInterface.askLetter()).thenReturn("a", "b", "c", "d", "f", "g", "h", "j", "k");
+        when(dictionary.getWords()).thenReturn(List.of("secret"));
+
+        hangmanGame.initiateGame();
+
+        verify(hangmanInterface, atLeast(6)).askLetter();
+        verify(hangmanInterface, atLeast(6)).drawInterface(anyInt());
+        verify(hangmanInterface).endGame(true);
+    }
+
+    @Test
+    void playGame_ShouldWarmAboutAlreadyUsedLetterAndLoseTheGame() {
+        when(hangmanInterface.askPersonToPlay()).thenReturn("Y", "N");
+        when(hangmanInterface.askLetter()).thenReturn("a", "a", "c", "d", "f", "g", "h", "j", "k");
+        when(dictionary.getWords()).thenReturn(List.of("secret"));
+
+        hangmanGame.initiateGame();
+
+        verify(hangmanInterface, atLeastOnce()).alreadySuggestedLetterWarning();
+        verify(hangmanInterface, atLeast(6)).askLetter();
+        verify(hangmanInterface, atLeast(6)).drawInterface(anyInt());
+        verify(hangmanInterface).endGame(true);
+    }
+
+    @Test
+    void playGame_ShouldWinTheGame() {
+        when(hangmanInterface.askPersonToPlay()).thenReturn("Y", "N");
+        when(hangmanInterface.askLetter()).thenReturn("s", "e", "c", "r", "t");
+        when(dictionary.getWords()).thenReturn(List.of("secret"));
+
+        hangmanGame.initiateGame();
+
+        verify(hangmanInterface, times(5)).askLetter();
+        verify(hangmanInterface, times(6)).drawInterface(0);
+        verify(hangmanInterface).endGame(false);
+    }
+
+    @Test
+    void endGame_ShouldInitiateTheGameAgain() {
+        doNothing().when(hangmanGame).initiateGame();
+
+        hangmanGame.endGame(anyBoolean());
+
+        verify(hangmanInterface).endGame(anyBoolean());
+        verify(hangmanGame).initiateGame();
+    }
+
+    @Test
+    void exitGame_ShouldExitTheGame() {
+        hangmanGame.exitGame();
+
+        verify(hangmanInterface).exitGame();
     }
 }
